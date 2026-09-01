836999c2b7a2a86c7663e9c1cba94b4dc9076140
Edwin <152198197+Usarmame@users.noreply.github.com>
2026-09-01 18:32:19 +0200

    Adicionando fila dinamica

---
+++ codigo-fonte/FilaDinamica/DesafioListaDinamica.java
@@ -0,0 +1,33 @@
+public class DesafioListaDinamica {
+
+    public static void main(String[] args) {
+        ListaDinamica1 lista = new ListaDinamica1();
+
+        lista.enqueue(10);
+        lista.enqueue(15);
+        lista.enqueue(20);
+        lista.enqueue(25);
+        lista.enqueue(30);
+
+        //Atividade 1:
+        lista.enqueue(40);
+
+        //Atividade 2:
+        System.out.println("Elemento removido: " +lista.dequeue());
+
+        //Atividade 3:
+        System.out.println("Elemento da frente: "+lista.front());
+
+        //Atividade 4:
+       lista.quantidadePar();
+
+       //Atividade 5:
+        System.out.println("A soma dos elementos vale: "+lista.somaElementos());
+
+        //Atividade 6:
+        System.out.println("Maior elemento: "+lista.maiorElemento());
+    }
+
+
+
+}
---
+++ codigo-fonte/FilaDinamica/ListaDinamica1.java
@@ -0,0 +1,193 @@
+
+
+public class ListaDinamica1 {
+    //Parte 1
+
+    private No inicio;
+    private No fim;
+    private int tamanho;
+
+    public ListaDinamica1(){
+        this.inicio = null;
+        this.fim = null;
+        this.tamanho = 0;
+    }
+
+    private static class No {
+
+        int valor;
+        No prox;
+
+        private No(int valor){
+            this.valor = valor;
+            this.prox = null;
+        }
+    }
+
+    public void enqueue(int element){
+        No novoNo = new No(element);
+
+        if (isEmpty()){
+            inicio = novoNo;
+        }else{
+            fim.prox = novoNo;
+        }
+
+        fim = novoNo;
+        tamanho++;
+    }
+
+    public int dequeue(){
+        if(isEmpty()){
+            throw new RuntimeException("Empty Queue!");
+        }
+
+        int valor = inicio.valor;
+        inicio = inicio.prox;
+        tamanho--;
+
+        if (inicio == null){
+            fim = null;
+        }
+
+        return valor;
+
+    }
+
+    public int front(){
+        if (isEmpty()){
+            throw new RuntimeException("Empty Queue!");
+        }
+
+        return inicio.valor;
+    }
+
+    public boolean isEmpty(){
+        return inicio == null;
+    }
+
+    public void displayQueue(){
+        if(isEmpty()){
+            System.out.println("Empty Queue!");
+            return;
+        }
+
+        No valorAtual = inicio;
+        System.out.print("Lista Ligada: { ");
+        while(valorAtual != null){
+            System.out.print(valorAtual.valor);
+            if(valorAtual.prox != null){
+                System.out.print(", ");
+            }
+            valorAtual = valorAtual.prox;
+        }
+        System.out.println(" }");
+    }
+
+    public int getTamanho(){
+        return tamanho;
+    }
+
+    public boolean isElement(int x){
+        if (isEmpty()){
+            System.out.println("A lista está vazia!");
+            return false;
+        }
+        No elemento = inicio;
+        while(elemento!=null){
+            if(elemento.valor == x){
+                return true;
+            }
+            elemento = elemento.prox;
+        }
+        return false;
+
+    }
+
+    public void quantidadePar(){
+        No valorAtual = inicio;
+        int contaPar = 0, contaImpar = 0;
+
+        if (isEmpty()){
+            System.out.println("A lista está vazia!");
+            return;
+        }
+        while(valorAtual != null){
+            if (valorAtual.valor % 2 == 0){
+                contaPar += 1;
+            }else{
+                contaImpar += 1;
+            }
+            valorAtual = valorAtual.prox;
+        }
+
+        System.out.println("Há ao todo "+ contaPar+ " pares e "+contaImpar+ " impares.");
+
+    }
+
+    public int maiorElemento(){
+        int max;
+        max = inicio.valor;
+
+         No valorAtual = inicio;
+
+        if (isEmpty()){
+            System.out.println("Lista vazia!");
+        }
+
+        while (valorAtual != null){
+            if(valorAtual.valor > max){
+                max = valorAtual.valor;
+            }
+
+            valorAtual = valorAtual.prox;
+        }
+
+        return max;
+
+        }
+
+    public int somaElementos(){
+        No valorAtual = inicio;
+        int somaElementos = 0;
+
+        if(isEmpty()){
+            System.out.println("Lista vazia!");
+            return -1;
+        }
+
+        while (valorAtual != null){
+            somaElementos += valorAtual.valor;
+
+            valorAtual = valorAtual.prox;
+        }
+
+        return somaElementos;
+    }
+
+    public static void main(String[] args) {
+        //Parte 1:
+        ListaDinamica1 listaLigada = new ListaDinamica1();
+
+        listaLigada.enqueue(10);
+        listaLigada.enqueue(12);
+        listaLigada.enqueue(13);
+        listaLigada.enqueue(14);
+        listaLigada.enqueue(17);
+        listaLigada.enqueue(9);
+
+        listaLigada.displayQueue();
+
+        System.out.println("A fila possui "+listaLigada.getTamanho()+" elementos armazenados.");
+
+        if(listaLigada.isElement(12)){
+            System.out.println("O valor é elemento da fila.");
+        }else{
+            System.out.println("O elemento não é elemento da fila.");
+        }
+
+        listaLigada.quantidadePar();
+
+        System.out.println("A soma dos elementos da lista vale: "+ listaLigada.somaElementos());
+    }
+}
