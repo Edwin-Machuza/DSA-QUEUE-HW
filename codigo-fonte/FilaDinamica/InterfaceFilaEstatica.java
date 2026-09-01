bd0e144ae5568fb19a3499d81ba285ece0e0b249
Gaudencio Alexandre Mapanzene <gaudencioalexandre1@gmail.com>
2026-09-01 20:30:35 +0200

    Clonado

---
+++ codigo-fonte/FilaDinamica/InterfaceFilaEstatica.java
@@ -0,0 +1,25 @@
+package util;
+
+public interface InterfaceFilaEstatica {
+
+    void inserirElemento(int value);
+
+    int removerElemento(int value);
+
+    int frente();
+
+    boolean filaVazia();
+
+    boolean filaCheia();
+
+    void percorrerFila();
+
+    int buscarElemento(int index);
+
+    void contarParesImpares();
+
+    void inverterVetor();
+
+    void maiorMenor();
+
+}
--- codigo-fonte/FilaDinamica/Main.java
+++ codigo-fonte/FilaDinamica/Main.java
@@ -1,10 +1,56 @@
-public class Main {
+import util.FilaEstatica;
 
-    public static void main(String[] args) {
-        Menu m = new Menu();
+void main(String...args) throws IOException {
 
-        m.menu();
-    }
+    InputStreamReader isr = new InputStreamReader(System.in);
+    BufferedReader br = new BufferedReader(isr);
+    FilaEstatica fe = new FilaEstatica(10);
+
+    int option, number;
+
+    do{
+        showMenu();
+        option = Integer.parseInt(br.readLine());
+
+        switch(option){
+
+            case 1: IO.print("Numero: ");
+                    number = Integer.parseInt(br.readLine());
+                    fe.inserirElemento(number);
+                    IO.println("Elemento adicionado com sucesso!");
+                    break;
+            case 2: IO.println("Numero: ");
+                    number = Integer.parseInt(br.readLine());
+                    fe.removerElemento(number);
+                    IO.println("Elemento removido com sucesso");
+                    break;
+            case 3: IO.print("Digite o indice: ");
+                    number = Integer.parseInt(br.readLine());
+                    IO.println(fe.buscarElemento(number));
+                    break;
+            case 4: fe.contarParesImpares();
+                    break;
+            case 5: fe.maiorMenor();
+                    break;
+            case 6: IO.println("+====+Inverter+====+");
+                    fe.inverterVetor();
+                    break;
 
 
+        }
+
+
+
+    }while(option != 3);
+
+}
+public static void showMenu(){
+    IO.println("1.Inserir");
+    IO.println("2.Remover");
+    IO.println("3.Busca pelo indice");
+    IO.println("4.Quantidade de pares e impares");
+    IO.println("5.Comparação");
+    IO.println("6.Inverter o vetor");
+
+    IO.print("Option: ");
 }
